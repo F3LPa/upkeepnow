e3b20cc5ce4352f478d86b720e1d4d33b24297ca
F3LPa <felipedasilvapereira153@gmail.com>
2025-03-23 19:17:57 -0300

    engine, models e session

---
+++ conf/db_session.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from pathlib import Path
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.future.engine import Engine
+
+from models.model_base import Base
+
+__engine: Optional[Engine] = None
+
+
+def create_engine(sqlite: bool = False) -> Engine:
+    """
+    configura conexao com BD
+    """
+    global __engine
+
+    if __engine:
+        return
+    
+    conn_str = "jdbc:mysql://localhost:3306/upkeepnow"
+    __engine = sa.create_engine(url=conn_str, echo=False)
+
+    return __engine
+
+def create_session() -> None:
+    global __engine
+
+    if not __engine:
+        create_engine()
+    
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+
+    return Session
+
+def create_tables() -> None:
+    global __engine
+
+    if not __engine:
+        create_engine()
+
+    import models.__all_models
+    Base.metadata.drop_all(__engine)
+    Base.metadata.create_all(__engine)
