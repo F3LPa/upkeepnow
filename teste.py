10fb355a02d327d7f5cf14a8d0a6cb99bbfc5a7f
F3LPa <felipedasilvapereira153@gmail.com>
2025-03-31 20:04:11 -0300

    config ruff

---
+++ teste.py
@@ -0,0 +1,17 @@
+import math
+
+def exemplo_funcao():
+    x = 10
+    y = 5
+    # Variável não utilizada
+    z = 7
+
+    # Não usa a variável 'z' e a função não retorna nada.
+    resultado = x + y * z
+    print("O resultado é:", resultado)
+
+# Variáveis com nomes que não seguem as convenções de nomenclatura do PEP8.
+myVar = 42
+MyAnotherVar = 100
+
+exemplo_funcao()
